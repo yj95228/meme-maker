dc1ca532bee996b92e4fe14894c0834b10be24d6
yj95228 <kei02119@naver.com>
2022-08-16 22:51:30 +0900

    2.0 Painting Lines

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_SIZE = 800;
 
-// 집
-ctx.fillRect(100, 200, 50, 200);
-ctx.fillRect(300, 200, 50, 200);
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#73fff5",
+  "#18dcff",
+  "#7d5fff",
+];
+
+const beginPoint = {
+  x: Math.random() * CANVAS_SIZE,
+  y: Math.random() * CANVAS_SIZE,
+};
+
 ctx.lineWidth = 2;
-ctx.fillRect(200, 300, 50, 100);
-ctx.fillRect(100, 200, 200, 20);
-ctx.moveTo(100, 200);
-ctx.lineTo(225, 100);
-ctx.lineTo(350, 200);
-ctx.fill();
-
-// 사람
-ctx.beginPath();
-ctx.fillRect(470, 250, 15, 100);
-ctx.fillRect(610, 250, 15, 100);
-ctx.fillRect(520, 250, 60, 100);
-ctx.arc(550, 180, 50, 0, 2 * Math.PI);
-ctx.fill();
-
-// 눈
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(570, 160, 10, Math.PI, 0);
-ctx.arc(530, 160, 10, Math.PI, 0);
-ctx.fill();
-ctx.beginPath();
-ctx.arc(570, 180, 5, 0, 2 * Math.PI);
-ctx.arc(530, 180, 5, 0, 2 * Math.PI);
-ctx.fill();
+function onMove({ offsetX, offsetY }) {
+  beginPoint.x = offsetX;
+  beginPoint.y = offsetY;
+}
+function onClick() {
+  ctx.beginPath();
+  ctx.moveTo(400, 400);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(beginPoint.x, beginPoint.y);
+  ctx.stroke();
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("click", onClick);
