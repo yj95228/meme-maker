1418a10be7c0222f6200f8c78846d647b828765a
yj95228 <kei02119@naver.com>
2022-08-16 21:57:31 +0900

    1.4-5 Draw house and person

--- app.js
+++ app.js
@@ -3,9 +3,32 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
+// 집
+ctx.fillRect(100, 200, 50, 200);
+ctx.fillRect(300, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.fillRect(200, 300, 50, 100);
+ctx.fillRect(100, 200, 200, 20);
+ctx.moveTo(100, 200);
+ctx.lineTo(225, 100);
+ctx.lineTo(350, 200);
+ctx.fill();
+
+// 사람
+ctx.beginPath();
+ctx.fillRect(470, 250, 15, 100);
+ctx.fillRect(610, 250, 15, 100);
+ctx.fillRect(520, 250, 60, 100);
+ctx.arc(550, 180, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+// 눈
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(570, 160, 10, Math.PI, 0);
+ctx.arc(530, 160, 10, Math.PI, 0);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(570, 180, 5, 0, 2 * Math.PI);
+ctx.arc(530, 180, 5, 0, 2 * Math.PI);
+ctx.fill();
