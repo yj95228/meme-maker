3ced628f2aa856ded8377d54f6479d04abd1a17e
yj95228 <kei02119@naver.com>
2022-08-17 22:17:15 +0900

    2.2 Line Width

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const CANVAS_SIZE = 800;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
-
-ctx.lineWidth = 2;
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+
 function onMove({ offsetX, offsetY }) {
   if (isPainting) {
     ctx.lineTo(offsetX, offsetY);
@@ -21,8 +21,16 @@ function startPainting() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
 }
+function onLineWidthChange(event) {
+  // console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
